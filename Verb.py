ef94144eb2b55b89db17b3016f331af7433d614e
Rahul Gupta <rguptaemail10@gmail.com>
2020-01-31 17:55:08 -0500

    Another Update

--- Verb.py
+++ Verb.py
@@ -10,11 +10,29 @@ class Verb:
     self.pluperfect = Tense()
     self.future_perfect = Tense()
 
+  def get_declaination(self, dec):
+    decs = {}
+    for key, tense in self.pack().items():
+      declination = [v[dec - 1] for v in tense.pack()]
+      decs[key] = declination
+    return decs
+
+  def pack(self):
+    return {
+      'present': self.present,
+      'imperfect': self.imperfect,
+      'future': self.future,
+      'perfect': self.perfect,
+      'pluperfect': self.pluperfect,
+      'future_perfect': self.future_perfect
+    }
+
 class Nounadj:
   def __init__(self, value):
     self.value = value
     self.singular = POS()
     self.plural = POS()
+
 
 class Tense:
   def __init__(self):
@@ -24,6 +42,10 @@ class Tense:
     self.we = []
     self.youpl = []
     self.they = []
+
+  def pack(self):
+    return self.me, self.yous, self.it, self.we, self.youpl, self.they
+
 
 class POS:
   def __init__(self):
@@ -48,144 +70,249 @@ quī = Nounadj("quī")
 
 
 #Nounadj Data
-ego.singular.nominative.append([''])
-ego.singular.genitive.append([''])
-ego.singular.dative.append([''])
-ego.singular.accusative.append([''])
-ego.singular.ablative.append([''])
+ego.singular.nominative = ['', '', '', '']
+ego.singular.genitive = ['', '', '', '']
+ego.singular.dative = ['', '', '', '']
+ego.singular.accusative = ['', '', '', '']
+ego.singular.ablative = ['', '', '', '']
 
-ego.plural.nominative.append([''])
-ego.plural.genitive.append([''])
-ego.plural.dative.append([''])
-ego.plural.accusative.append([''])
-ego.plural.ablative.append([''])
+ego.plural.nominative = ['', '', '', '']
+ego.plural.genitive = ['', '', '', '']
+ego.plural.dative = ['', '', '', '']
+ego.plural.accusative = ['', '', '', '']
+ego.plural.ablative = ['', '', '', '']
 
-sē.singular.nominative.append([''])
-sē.singular.genitive.append([''])
-sē.singular.dative.append([''])
-sē.singular.accusative.append([''])
-sē.singular.ablative.append([''])
+sē.singular.nominative = ['', '', '', '']
+sē.singular.genitive = ['', '', '', '']
+sē.singular.dative = ['', '', '', '']
+sē.singular.accusative = ['', '', '', '']
+sē.singular.ablative = ['', '', '', '']
 
-sē.plural.nominative.append([''])
-sē.plural.genitive.append([''])
-sē.plural.dative.append([''])
-sē.plural.accusative.append([''])
-sē.plural.ablative.append([''])
+sē.plural.nominative = ['', '', '', '']
+sē.plural.genitive = ['', '', '', '']
+sē.plural.dative = ['', '', '', '']
+sē.plural.accusative = ['', '', '', '']
+sē.plural.ablative = ['', '', '', '']
 
-hesheit.singular.nominative.append([''])
-hesheit.singular.genitive.append([''])
-hesheit.singular.dative.append([''])
-hesheit.singular.accusative.append([''])
-hesheit.singular.ablative.append([''])
+hesheit.singular.nominative = ['', '', '', '']
+hesheit.singular.genitive = ['', '', '', '']
+hesheit.singular.dative = ['', '', '', '']
+hesheit.singular.accusative = ['', '', '', '']
+hesheit.singular.ablative = ['', '', '', '']
 
-hesheit.plural.nominative.append([''])
-hesheit.plural.genitive.append([''])
-hesheit.plural.dative.append([''])
-hesheit.plural.accusative.append([''])
-hesheit.plural.ablative.append([''])
+hesheit.plural.nominative = ['', '', '', '']
+hesheit.plural.genitive = ['', '', '', '']
+hesheit.plural.dative = ['', '', '', '']
+hesheit.plural.accusative = ['', '', '', '']
+hesheit.plural.ablative = ['', '', '', '']
 
-hic.singular.nominative.append([''])
-hic.singular.genitive.append([''])
-hic.singular.dative.append([''])
-hic.singular.accusative.append([''])
-hic.singular.ablative.append([''])
+hic.singular.nominative = ['', '', '', '']
+hic.singular.genitive = ['', '', '', '']
+hic.singular.dative = ['', '', '', '']
+hic.singular.accusative = ['', '', '', '']
+hic.singular.ablative = ['', '', '', '']
 
-hic.plural.nominative.append([''])
-hic.plural.genitive.append([''])
-hic.plural.dative.append([''])
-hic.plural.accusative.append([''])
-hic.plural.ablative.append([''])
+hic.plural.nominative = ['', '', '', '']
+hic.plural.genitive = ['', '', '', '']
+hic.plural.dative = ['', '', '', '']
+hic.plural.accusative = ['', '', '', '']
+hic.plural.ablative = ['', '', '', '']
 
-ille.singular.nominative.append([''])
-ille.singular.genitive.append([''])
-ille.singular.dative.append([''])
-ille.singular.accusative.append([''])
-ille.singular.ablative.append([''])
+ille.singular.nominative = ['', '', '', '']
+ille.singular.genitive = ['', '', '', '']
+ille.singular.dative = ['', '', '', '']
+ille.singular.accusative = ['', '', '', '']
+ille.singular.ablative = ['', '', '', '']
 
-ille.plural.nominative.append([''])
-ille.plural.genitive.append([''])
-ille.plural.dative.append([''])
-ille.plural.accusative.append([''])
-ille.plural.ablative.append([''])
+ille.plural.nominative = ['', '', '', '']
+ille.plural.genitive = ['', '', '', '']
+ille.plural.dative = ['', '', '', '']
+ille.plural.accusative = ['', '', '', '']
+ille.plural.ablative = ['', '', '', '']
 
-ipse.singular.nominative.append([''])
-ipse.singular.genitive.append([''])
-ipse.singular.dative.append([''])
-ipse.singular.accusative.append([''])
-ipse.singular.ablative.append([''])
+ipse.singular.nominative = ['', '', '', '']
+ipse.singular.genitive = ['', '', '', '']
+ipse.singular.dative = ['', '', '', '']
+ipse.singular.accusative = ['', '', '', '']
+ipse.singular.ablative = ['', '', '', '']
 
-ipse.plural.nominative.append([''])
-ipse.plural.genitive.append([''])
-ipse.plural.dative.append([''])
-ipse.plural.accusative.append([''])
-ipse.plural.ablative.append([''])
+ipse.plural.nominative = ['', '', '', '']
+ipse.plural.genitive = ['', '', '', '']
+ipse.plural.dative = ['', '', '', '']
+ipse.plural.accusative = ['', '', '', '']
+ipse.plural.ablative = ['', '', '', '']
 
-īdem.singular.nominative.append([''])
-īdem.singular.genitive.append([''])
-īdem.singular.dative.append([''])
-īdem.singular.accusative.append([''])
-īdem.singular.ablative.append([''])
+īdem.singular.nominative = ['', '', '', '']
+īdem.singular.genitive = ['', '', '', '']
+īdem.singular.dative = ['', '', '', '']
+īdem.singular.accusative = ['', '', '', '']
+īdem.singular.ablative = ['', '', '', '']
 
-īdem.plural.nominative.append([''])
-īdem.plural.genitive.append([''])
-īdem.plural.dative.append([''])
-īdem.plural.accusative.append([''])
-īdem.plural.ablative.append([''])
+īdem.plural.nominative = ['', '', '', '']
+īdem.plural.genitive = ['', '', '', '']
+īdem.plural.dative = ['', '', '', '']
+īdem.plural.accusative = ['', '', '', '']
+īdem.plural.ablative = ['', '', '', '']
 
-quī.singular.nominative.append([''])
-quī.singular.genitive.append([''])
-quī.singular.dative.append([''])
-quī.singular.accusative.append([''])
-quī.singular.ablative.append([''])
+quī.singular.nominative = ['', '', '', '']
+quī.singular.genitive = ['', '', '', '']
+quī.singular.dative = ['', '', '', '']
+quī.singular.accusative = ['', '', '', '']
+quī.singular.ablative = ['', '', '', '']
 
-quī.plural.nominative.append([''])
-quī.plural.genitive.append([''])
-quī.plural.dative.append([''])
-quī.plural.accusative.append([''])
-quī.plural.ablative.append([''])
+quī.plural.nominative = ['', '', '', '']
+quī.plural.genitive = ['', '', '', '']
+quī.plural.dative = ['', '', '', '']
+quī.plural.accusative = ['', '', '', '']
+quī.plural.ablative = ['', '', '', '']
 
-#Verb Data
+#Verb Data 
 
 #Checked
 RegularActive.present.me = ['portō', 'doceō', 'trahō', 'audiō']
 RegularActive.present.yous = ['portās', 'docēs', 'trahis', 'audīs']
-RegularActive.present.it.append(['portat', 'docet', 'trahit', 'audit'])
-RegularActive.present.we.append(['portāmus', 'docēmus', 'trahimus', 'audīmus'])
-RegularActive.present.youpl.append(['portātis', 'docētis', 'trahitis', 'audītis'])
-RegularActive.present.they.append(['portant', 'docent', 'trahunt', 'audiunt'])
+RegularActive.present.it = ['portat', 'docet', 'trahit', 'audit']
+RegularActive.present.we = ['portāmus', 'docēmus', 'trahimus', 'audīmus']
+RegularActive.present.youpl = ['portātis', 'docētis', 'trahitis', 'audītis']
+RegularActive.present.they = ['portant', 'docent', 'trahunt', 'audiunt']
 
 #
-RegularActive.imperfect.me.append(['portābam', 'docēbam', 'trahēbam', 'audiēbam'])
-RegularActive.imperfect.yous.append(['portābās', 'docēbās', 'trahēbās', 'audiēbās'])
-RegularActive.imperfect.it.append(['portābat', 'docēbat', 'trahēbat', 'audiēbat'])
-RegularActive.imperfect.we.append(['portābāmus', 'docēbāmus', 'trahēbāmus', 'audiēbāmus'])
-RegularActive.imperfect.youpl.append(['portābātis', 'docēbātis', 'trahēbātis', 'audiēbātis'])
-RegularActive.imperfect.they.append(['portābant', 'docēbant', 'trahēbant', 'audiēbant'])
+RegularActive.imperfect.me = ['portābam', 'docēbam', 'trahēbam', 'audiēbam']
+RegularActive.imperfect.yous = ['portābās', 'docēbās', 'trahēbās', 'audiēbās']
+RegularActive.imperfect.it = ['portābat', 'docēbat', 'trahēbat', 'audiēbat']
+RegularActive.imperfect.we = ['portābāmus', 'docēbāmus', 'trahēbāmus', 'audiēbāmus']
+RegularActive.imperfect.youpl = ['portābātis', 'docēbātis', 'trahēbātis', 'audiēbātis']
+RegularActive.imperfect.they = ['portābant', 'docēbant', 'trahēbant', 'audiēbant']
 
 #
-RegularActive.future.me.append(['portābō', 'docēbō', 'traham', 'audiam'])
-RegularActive.future.yous.append(['portābis', 'docēbis', 'trahēs', 'audiēs'])
-RegularActive.future.it.append(['portābit', 'docēbit', 'trahet', 'audiet'])
-RegularActive.future.we.append(['portābimus', 'docēbimus', 'trahētis', 'audiētis'])
-RegularActive.future.youpl.append(['portābitis', 'docēbitis', 'trahētis', 'audiētis'])
-RegularActive.future.they.append(['portābunt', 'docēbunt', 'trahent', 'audient'])
+RegularActive.future.me = ['portābō', 'docēbō', 'traham', 'audiam']
+RegularActive.future.yous = ['portābis', 'docēbis', 'trahēs', 'audiēs']
+RegularActive.future.it = ['portābit', 'docēbit', 'trahet', 'audiet']
+RegularActive.future.we = ['portābimus', 'docēbimus', 'trahētis', 'audiētis']
+RegularActive.future.youpl = ['portābitis', 'docēbitis', 'trahētis', 'audiētis']
+RegularActive.future.they = ['portābunt', 'docēbunt', 'trahent', 'audient']
 
 #
-RegularActive.perfect.me.append([''])
-RegularActive.perfect.yous.append([''])
-RegularActive.perfect.it.append
+RegularActive.perfect.me = ['portāvī', 'docuī', 'trāxī', 'audīvī']
+RegularActive.perfect.yous = ['portāvistī', 'docuistī', 'trāxistī', 'audīvistī']
+RegularActive.perfect.it = ['portāvit', '', '', '']
+RegularActive.perfect.we = ['', '', '', '']
+RegularActive.perfect.youpl = ['', '', '', '']
+RegularActive.perfect.they = ['', '', '', '']
 
+RegularActive.pluperfect.me = ['', '', '', '']
+RegularActive.pluperfect.yous = ['', '', '', '']
+RegularActive.pluperfect.it = ['', '', '', '']
+RegularActive.pluperfect.we = ['', '', '', '']
+RegularActive.pluperfect.youpl = ['', '', '', '']
+RegularActive.pluperfect.they = ['', '', '', '']
 
-RegularPassive.present.me.append(['portor', 'doceor', 'trahor', 'audior'])
+RegularActive.future_perfect.me = ['', '', '', '']
+RegularActive.future_perfect.yous = ['', '', '', '']
+RegularActive.future_perfect.it = ['', '', '', '']
+RegularActive.future_perfect.we = ['', '', '', '']
+RegularActive.future_perfect.youpl = ['', '', '', '']
+RegularActive.future_perfect.they = ['', '', '', '']
 
-Deponent.present.me.append(['cōnor', 'vereor', 'loquor', 'orior'])
+#Regular Passive Verb Data
+RegularPassive.present.me = ['portor', 'doceor', 'trahor', 'audior']
+RegularPassive.present.yous = ['', '', '', '']
+RegularPassive.present.it = ['', '', '', '']
+RegularPassive.present.we = ['', '', '', '']
+RegularPassive.present.youpl = ['', '', '', '']
+RegularPassive.present.they = ['', '', '', '']
 
-print(RegularActive.present.me)
+RegularPassive.imperfect.me = ['', '', '', '']
+RegularPassive.imperfect.yous = ['', '', '', '']
+RegularPassive.imperfect.it = ['', '', '', '']
+RegularPassive.imperfect.we = ['', '', '', '']
+RegularPassive.imperfect.youpl = ['', '', '', '']
+RegularPassive.imperfect.they = ['', '', '', '']
+
+RegularPassive.future.me = ['', '', '', '']
+RegularPassive.future.yous = ['', '', '', '']
+RegularPassive.future.it = ['', '', '', '']
+RegularPassive.future.we = ['', '', '', '']
+RegularPassive.future.youpl = ['', '', '', '']
+RegularPassive.future.they = ['', '', '', '']
+
+RegularPassive.perfect.me = ['', '', '', '']
+RegularPassive.perfect.yous = ['', '', '', '']
+RegularPassive.perfect.it = ['', '', '', '']
+RegularPassive.perfect.we = ['', '', '', '']
+RegularPassive.perfect.youpl = ['', '', '', '']
+RegularPassive.perfect.they = ['', '', '', '']
+
+RegularPassive.pluperfect.me = ['', '', '', '']
+RegularPassive.pluperfect.yous = ['', '', '', '']
+RegularPassive.pluperfect.it = ['', '', '', '']
+RegularPassive.pluperfect.we = ['', '', '', '']
+RegularPassive.pluperfect.youpl = ['', '', '', '']
+RegularPassive.pluperfect.they = ['', '', '', '']
+
+RegularPassive.future_perfect.me = ['', '', '', '']
+RegularPassive.future_perfect.yous = ['', '', '', '']
+RegularPassive.future_perfect.it = ['', '', '', '']
+RegularPassive.future_perfect.we = ['', '', '', '']
+RegularPassive.future_perfect.youpl = ['', '', '', '']
+RegularPassive.future_perfect.they = ['', '', '', '']
+
+#Deponent Verbs
+
+Deponent.present.me = ['cōnor', 'vereor', 'loquor', 'orior']
+Deponent.present.yous = ['', '', '', '']
+Deponent.present.it = ['', '', '', '']
+Deponent.present.we = ['', '', '', '']
+Deponent.present.youpl = ['', '', '', '']
+Deponent.present.they = ['', '', '', '']
+
+Deponent.imperfect.me = ['', '', '', '']
+Deponent.imperfect.yous = ['', '', '', '']
+Deponent.imperfect.it = ['', '', '', '']
+Deponent.imperfect.we = ['', '', '', '']
+Deponent.imperfect.youpl = ['', '', '', '']
+Deponent.imperfect.they = ['', '', '', '']
+
+Deponent.future.me = ['', '', '', '']
+Deponent.future.yous = ['', '', '', '']
+Deponent.future.it = ['', '', '', '']
+Deponent.future.we = ['', '', '', '']
+Deponent.future.youpl = ['', '', '', '']
+Deponent.future.they = ['', '', '', '']
+
+Deponent.perfect.me = ['', '', '', '']
+Deponent.perfect.yous = ['', '', '', '']
+Deponent.perfect.it = ['', '', '', '']
+Deponent.perfect.we = ['', '', '', '']
+Deponent.perfect.youpl = ['', '', '', '']
+Deponent.perfect.they = ['', '', '', '']
+
+Deponent.pluperfect.me = ['', '', '', '']
+Deponent.pluperfect.yous = ['', '', '', '']
+Deponent.pluperfect.it = ['', '', '', '']
+Deponent.pluperfect.we = ['', '', '', '']
+Deponent.pluperfect.youpl = ['', '', '', '']
+Deponent.pluperfect.they = ['', '', '', '']
+
+Deponent.future_perfect.me = ['', '', '', '']
+Deponent.future_perfect.yous = ['', '', '', '']
+Deponent.future_perfect.it = ['', '', '', '']
+Deponent.future_perfect.we = ['', '', '', '']
+Deponent.future_perfect.youpl = ['', '', '', '']
+Deponent.future_perfect.they = ['', '', '', '']
+
+#print(RegularActive.present.me)
 
 #Algorithm
 user_input = RegularActive.present
 
 # print(RegularActive.present)
 
+print(RegularActive.get_declaination(1))
+
 # print(RegularActive.present.me)
 # print(RegularActive.imperfect.me)
+# x = 0
+# for i in range(0, len(user_input))
+#   print(RegularActive.future.me[x])
+#   x+=1
